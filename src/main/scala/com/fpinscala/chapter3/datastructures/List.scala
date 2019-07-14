5dc932f2d7cd8060252000aff9f6826ca55492cd
Seeta Ramayya Vadali <3521036+Seetaramayya@users.noreply.github.com>
2019-07-14 22:22:43 +0200

    Completed exercise up to 3.15 (including)

--- src/main/scala/com/fpinscala/chapter3/datastructures/List.scala
+++ src/main/scala/com/fpinscala/chapter3/datastructures/List.scala
@@ -1,5 +1,7 @@
 package com.fpinscala.chapter3.datastructures
 
+import scala.annotation.tailrec
+
 
 // `List` data type, parameterized on a type, `A`
 sealed trait List[+A]
@@ -36,8 +38,10 @@ object List {
   }
 
   def sum2(ns: List[Int]): Int = foldRight(ns, 0)((x, y) => x + y)
+  def sum3(ns: List[Int]): Int = foldLeft(ns, 0)((x, y) => x + y)
 
   def product2(ns: List[Double]): Double =  foldRight(ns, 1.0)(_ * _)
+  def product3(ns: List[Double]): Double =  foldLeft(ns, 1.0)(_ * _)
 
   def tail[A](l: List[A]): List[A] = l match {
     case Nil           => throw new UnsupportedOperationException("tail of empty list is not allowed")
@@ -49,15 +53,52 @@ object List {
     case Cons(_, xs) => Cons(h, xs)
   }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  @tailrec
+  def drop[A](l: List[A], n: Int): List[A] = if (n <= 0 || l == Nil) l else drop(tail(l), n - 1)
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  @tailrec
+  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, tail) if f(h) => Cons(h, tail)
+    case Cons(_, tail) => dropWhile(tail)(f)
+  }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, Nil) => Nil
+    case Cons(h, tail) => Cons(h, init(tail))
+  }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = l match {
+    case Nil => 0
+    case Cons(_, tail) => length(tail) + 1
+  }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  def length2[A](l: List[A]): Int = foldRight(l, 0)((_, acc) => acc + 1)
+  def length3[A](l: List[A]): Int = foldLeft(l, 0)((acc, _) => acc + 1)
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  @tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(h, t) => foldLeft(t, f(z, h))(f)
+  }
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = l match {
+    case Nil => Nil
+    case Cons(h, t) => Cons(f(h), map(t)(f))
+  }
+
+  def reverse[A](l: List[A]): List[A] = foldLeft(l, List[A]())((acc, elem) => Cons(elem, acc))
+
+  def foldLeftViaFoldRight[A, B](l: List[A], z: B)(f: (B, A) => B): B = {
+    foldRight(l, (identity: B) => identity)((a, g) => b => g(f(b, a)))(z)
+  }
+
+  def foldRightViaFoldLeft[A, B](l: List[A], z: B)(f: (A, B) => B): B = {
+    foldLeft(l, (identity: B) => identity)((g, a) => b => g(f(a, b)))(z)
+  }
+
+  def append2[A](l1: List[A], l2: List[A]): List[A] = foldRight(l1, l2)((elem, acc) => Cons(elem, acc))
+
+  def concat[A](l: List[List[A]]): List[A] = foldRight(l, List[A]())(append2)
 }
--- src/test/scala/com/fpinscala/chapter3/datastructures/ListSpec.scala
+++ src/test/scala/com/fpinscala/chapter3/datastructures/ListSpec.scala
@@ -22,5 +22,99 @@ class ListSpec extends WordSpec with Matchers {
       setHead(Nil, 4) shouldBe List(4)
       setHead(List(1, 2, 3), 4) shouldBe List(4, 2, 3)
     }
+
+    "return remaining elements after removing given number of elements for: '#drop'" in {
+      drop(Nil, 4) shouldBe Nil
+      drop(List(1, 2, 3), 1) shouldBe List(2, 3)
+      drop(List(1, 2, 3), 2) shouldBe List(3)
+      drop(List(1, 2, 3), 3) shouldBe Nil
+    }
+
+    "drop the elements while the given condition is met for: '#dropWhile'" in {
+      dropWhile(List(1, 2, 3))(_ => false) shouldBe Nil
+      dropWhile(List(1, 2, 3))(_ => true) shouldBe List(1, 2, 3)
+      dropWhile(List(1, 2, 3))( _ == 3) shouldBe List(3)
+      dropWhile(Nil)( _ => true) shouldBe Nil
+    }
+
+    "return all elements except last element for: '#init'" in {
+      init(List(1, 2, 3)) shouldBe List(1, 2)
+      init(List(1, 2)) shouldBe List(1)
+      init(List(1)) shouldBe Nil
+      init(Nil) shouldBe Nil
+    }
+
+    "return number of elements in the list for: '#length'" in {
+      List.length(List(1, 2, 3)) shouldBe 3
+      List.length2(List(1, 2, 3)) shouldBe 3
+      List.length3(List(1, 2, 3)) shouldBe 3
+
+      List.length(List(1, 2)) shouldBe 2
+      List.length2(List(1, 2)) shouldBe 2
+      List.length3(List(1, 2)) shouldBe 2
+    }
+
+    "return number of elements in the list for: '#foldLeft'" in {
+      foldLeft(List(1, 2, 3), 0)(_ + _) shouldBe 6
+      foldLeft(List(1, 2, 3), 0)(_ * _) shouldBe 0
+      foldLeft(List(1, 2, 3), 1)(_ * _) shouldBe 6
+      foldLeft(List(1, 2, 3), List[Int]())((acc, elem) => Cons(elem, acc)) shouldBe List(3, 2, 1)
+    }
+
+    "return number of elements in the list for: '#foldRight'" in {
+      foldRight(List(1, 2, 3), 0)(_ + _) shouldBe 6
+      foldRight(List(1, 2, 3), 0)(_ * _) shouldBe 0
+      foldRight(List(1, 2, 3), 1)(_ * _) shouldBe 6
+      foldRight(List(1, 2, 3), List[Int]())((elem, acc) => Cons(elem, acc)) shouldBe List(1, 2, 3)
+    }
+
+    "return number of elements in the list for: '#foldLeftViaFoldRight'" in {
+      foldLeftViaFoldRight(List(1, 2, 3), List[Int]())((acc, elem) => Cons(elem, acc)) shouldBe List(3, 2, 1)
+      foldLeftViaFoldRight(List(1, 2, 3), 0)(_ * _) shouldBe 0
+      foldLeftViaFoldRight(List(1, 2, 3), 1)(_ * _) shouldBe 6
+    }
+
+    "return number of elements in the list for: '#foldRightViaFoldLeft'" in {
+      foldRightViaFoldLeft(List(1, 2, 3), 0)(_ + _) shouldBe 6
+      foldRightViaFoldLeft(List(1, 2, 3), 0)(_ * _) shouldBe 0
+      foldRightViaFoldLeft(List(1, 2, 3), 1)(_ * _) shouldBe 6
+      foldRightViaFoldLeft(List(1, 2, 3), List[Int]())((elem, acc) => Cons(elem, acc)) shouldBe List(1, 2, 3)
+    }
+
+    "return number of elements in the list for: '#map'" in {
+      map(List(1, 2, 3))(_ + 1) shouldBe List(2, 3, 4)
+      map(List(1, 2, 3))(_ * 2) shouldBe List(2, 4, 6)
+      map(List[Int]())(_ * 2) shouldBe Nil
+    }
+
+    "return sum of elements in the list for: '#sum3'" in {
+      sum3(List(1, 2, 3)) shouldBe 6
+      sum3(List(1, 2)) shouldBe 3
+      sum3(List(1)) shouldBe 1
+      sum3(List()) shouldBe 0
+    }
+
+    "return sum of elements in the list for: '#product3'" in {
+      product3(List(1, 2, 3)) shouldBe 6.0
+      product3(List(1, 2)) shouldBe 2.0
+      product3(List(1)) shouldBe 1.0
+      product3(List()) shouldBe 1.0
+    }
+
+    "return reverse of the given list for: '#reverse'" in {
+      reverse(List()) shouldBe List()
+      reverse(List(1)) shouldBe List(1)
+      reverse(List(1, 2, 3)) shouldBe List(3, 2, 1)
+    }
+
+    "return appended list for given lists for: '#append2'" in {
+      append2(List(1, 2, 3), List(4, 5, 6)) shouldBe List(1, 2, 3, 4, 5, 6)
+      append2(List(1, 2, 3), Nil) shouldBe List(1, 2, 3)
+      append2(Nil, List(4, 5, 6)) shouldBe List(4, 5, 6)
+    }
+
+    "return flatten list : '#concat'" in {
+      concat(List(List(1, 2), List(3), List(4, 5, 6))) shouldBe List(1, 2, 3, 4, 5, 6)
+    }
   }
 }
